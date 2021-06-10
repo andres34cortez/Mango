191edebde4871b335b158e410339dbd896b7439c
Bruno Bianchi <bianchi.b95@gmail.com>
2021-06-10 15:55:30 -0300

    NavLink color

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Box } from "jsxstyle";
 import "./NavBar.css";
@@ -6,21 +6,43 @@ import "./NavBar.css";
 import Logo from "../../assets/Logo.png";
 
 const NavBar = () => {
+  const [active, serActive] = useState("/");
+
   return (
     <div className="navContainer">
       <div className="row">
         <img src={Logo} alt="" className="logo" />
         <Row>
-          <Link to="/" className="navLink">
+          <Link
+            to="/"
+            className="navLink"
+            style={{ color: active === "/" ? "#00ff66" : "#f5f5f5" }}
+            onClick={() => serActive("/")}
+          >
             <p>Inicio</p>
           </Link>
-          <Link to="/servicios" className="navLink">
+          <Link
+            to="/servicios"
+            className="navLink"
+            style={{ color: active === "/servicios" ? "#00ff66" : "#f5f5f5" }}
+            onClick={() => serActive("/servicios")}
+          >
             <p>Servicios</p>
           </Link>
-          <Link to="/proyectos" className="navLink">
+          {/* <Link
+            to="/proyectos"
+            className="navLink"
+            style={{ color: active === "/proyectos" ? "#00ff66" : "#f5f5f5" }}
+            onClick={() => serActive("/proyectos")}
+          >
             <p>Proyectos</p>
-          </Link>
-          <Link to="/contacto" className="navLink">
+          </Link> */}
+          <Link
+            to="/contacto"
+            className="navLink"
+            style={{ color: active === "/contacto" ? "#00ff66" : "#f5f5f5" }}
+            onClick={() => serActive("/contacto")}
+          >
             <p>Contacto</p>
           </Link>
         </Row>
