792ba10c0b9d4ae08e31e104ac4bbb79c8edc410
Bruno Bianchi <bianchi.b95@gmail.com>
2021-06-11 16:10:04 -0300

    Button

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -3,6 +3,7 @@ import "./button.css";
 
 interface Props {
   text?: string;
+  comentario?: string;
   top?: string;
   left?: string;
   onClick?: () => void;
@@ -19,7 +20,7 @@ const Button = (props: Props) => {
       }}
     >
       <h1 className="texto"> {props.text}</h1>
-      <h3>Agenda una llamada para que podamos asesorarte</h3>
+      <h3>{props.comentario}</h3>
     </div>
   );
 };
--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -159,6 +159,7 @@ const Home = () => {
           text="Agendar"
           top="40vw"
           left="40vw"
+          comentario="Agenda una llamada para que podamos asesorarte"
           onClick={() => setOpenModal((o) => !o)}
         />
         {/* /////////////////////MODAL///////////////////// */}
